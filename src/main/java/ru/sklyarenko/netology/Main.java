47e16677d5b25cd93d79c0165345d179a8daa2b9
Sklyarenko <relax@all-nevermind.ru>
2025-05-01 05:18:24 +0300

    refactor;

--- src/main/java/ru/sklyarenko/netology/Main.java
+++ src/main/java/ru/sklyarenko/netology/Main.java
@@ -1,7 +1,5 @@
 package ru.sklyarenko.netology;
 
-import ru.sklyarenko.netology.homework.lesson1.Introduction;
-import ru.sklyarenko.netology.homework.lesson2.Structure;
 import ru.sklyarenko.netology.homework.lesson3.Loops;
 
 public class Main {
@@ -11,8 +9,8 @@ public class Main {
      * При проверке конкретного ДЗ, вызов прочих можно просто закоментить и оно не будет запускаться и мешаться.
      */
     public static void main(String[] args) {
-        Introduction.printSolution(); // Homework for block № 1: Introduction to Java Programming
-        Structure.printSolution(); // Homework for block № 2: Program structure
+//        Introduction.printSolution(); // Homework for block № 1: Introduction to Java Programming
+//        Structure.printSolution(); // Homework for block № 2: Program structure
         Loops.printSolution();//Homework for block № 3: Conditional operators and loops
         //Homework for block № 4: Data types in Java: primitives
         //Homework for block № 5: Data types in Java: objects
